4697d10c7d4839d82d0307f4d1fc26fa51557a52
Cloud User <centos@ip-172-31-43-234.ap-southeast-1.compute.internal>
2020-06-08 14:08:17 +0000

    solution to Ex1

--- Ex1/top.v
+++ Ex1/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #1 
-// Student Name:
-// Date: 
+// Student Name:Ho Lee
+// Date: 08/06/2020
 //
 //  Description: In this exercise, you need to design a logical unit, where the  
 //  output acts according to the following truth table:
@@ -30,6 +30,17 @@ module logicalunit(
     );
     
     wire   out;
+
+    //wire   [1:0] ab =={a,b};
+    //assign out= (ab== 2b'00)? func[0]:
+		//(ab== 2b'01)? func[1]:
+		//(ab== 2b'10)? func[2]:
+		//func[3];
+
+     assign out = (func == 4b'1110)? a|b:
+		  (func == 4b'0110)? a^b:
+		  (func == 4b'1000)? a&b;
+	    
          
       //Todo: add you logic here
 
