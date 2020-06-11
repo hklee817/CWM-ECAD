995abdf737d04ccaa056bb7c0dd423d7e8b5846d
Cloud User <centos@ip-172-31-43-234.ap-southeast-1.compute.internal>
2020-06-11 10:10:37 +0000

    solution to Ex5

--- Ex5/top.v
+++ Ex5/top.v
@@ -13,3 +13,17 @@
 //           red, amber, green
 //////////////////////////////////////////////////////////////////////////////////
 
+module traffic light ( 
+input clk,
+output reg red,
+output reg amber,
+output reg green
+)
+
+always @ ( posedge clk) begin
+	case ({red , amber, green })     3b'100: red <=1 , amber <= 1, green <=0 ; 
+				        3b'110: red <=0, amber <=0, green<=1;
+					3b'001: red <=0, amber <=1, green<=1;
+					default: red <=0, amber <=0, green<=1;
+	endcase
+end
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -6,4 +6,37 @@
 // Description: A testbench module to test Ex5 - Traffic Lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+reg clk;
+reg  err;
+wire red , amber, green;
 
+parameter CLK_PERIOD = 10
+timescale 1ns/100ps
+
+initial begin 
+	clk =0 ;
+	err=0;
+	reg [2,0] light = {red, amber, green};
+
+		forever begin 
+			if ( light != 3b'100 || light != 3b'110|| light != 3b'001|| light != 3b'011) 
+				err= 1;
+				$display ("****UNWANTED LIGHT****");
+		end
+
+end
+
+initial begin 
+	#50
+	if (err==0 )
+		$display ("TEST PASSD!");
+	
+		$finish;
+end		
+
+trafficlight  top (
+input clk,
+output reg red, amber, green
+)
+
+endmodule	
